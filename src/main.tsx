27b9a4791e41a54de6aa45c10141af51186bf977
nebulaBdj <dongdung5070@gmail.com>
2024-08-26 21:59:01 +0900

    Init: 추가 Tanstack Query 설정
    ueryClient, QueryClientProvider 설정과 postcss cjs 파일 변환

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryCilent = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryCilent}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </QueryClientProvider>
+);
