670401d2f26c145c4f55daffbd573a552549bdaa
nebulaBdj <dongdung5070@gmail.com>
2024-08-27 08:06:48 +0000

    Init: 추가 svgr 라이브러리 설치 및 설정
    svg.d.ts 파일 생성, vite.config.ts, tsconfig.app&node.json, package.json 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import {Lets}  from "./assets/svg"
 
 export default function App() {
   return (
---
+++ src/assets/svg/index.ts
@@ -0,0 +1,1 @@
+export { default as Lets } from "./Lets";
