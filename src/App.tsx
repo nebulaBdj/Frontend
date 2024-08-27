9af241dac51c3d0c26f2bc6cb10835f3f42e8926
nebulaBdj <dongdung5070@gmail.com>
2024-08-27 06:09:24 +0000

    Init: 추가 배포 테스트를 위한 텍스트

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function App() {
   return (
     <>
-      <div>nav 바</div>
+      <div>nav 바바바바바바바ㅏㅈ</div>
       <Outlet />
       <div>footer</div>
     </>
