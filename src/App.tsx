4eab65e7e37d254548cacacaabe979d38a071077
nebulaBdj <dongdung5070@gmail.com>
2024-09-01 17:27:06 +0000

    Init: 추가 프리티어와 커스텀 컬러 설정, 변경 Ts 설정 파일,
    프리티어 설정 후 ts 설정 파일 통합, 디자인 커스텀 컬러 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Outlet } from "react-router-dom";
-import { useState } from "react";
-import Navigation from "./components/Navigations/Navigation"
-import Footer from "./components/Footer/Footer";
+import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import Navigation from './components/Navigations/Navigation';
+import Footer from './components/Footer/Footer';
 
 export default function App() {
-
   return (
     <div className="w-[95%] mx-auto">
       <Navigation></Navigation>
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/router.tsx";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/router.tsx';
+import './index.css';
 
 const queryCilent = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryCilent}>
     <StrictMode>
       <RouterProvider router={router} />
     </StrictMode>
-  </QueryClientProvider>
+  </QueryClientProvider>,
 );
