6c65d7cbff442afb5217fa7db894769e0824b309
nebulaBdj <dongdung5070@gmail.com>
2024-09-07 02:53:52 +0000

    Fix: 고침 프로그램 타입 필터 갤러시 깨짐 현상
    Fixes: #31

--- src/components/Filters/TypeFilter.tsx
+++ src/components/Filters/TypeFilter.tsx
@@ -13,7 +13,7 @@ const items = [
 const TypeFilter: React.FC<Props> = ({ activeItems, setActiveItems }) => {
   const handleClick = (programType: string) => {
     if (activeItems.includes(programType)) {
-      setActiveItems(activeItems.filter(item => item !== programType));
+      setActiveItems(activeItems.filter((item) => item !== programType));
     } else {
       setActiveItems([...activeItems, programType]);
     }
@@ -26,7 +26,7 @@ const TypeFilter: React.FC<Props> = ({ activeItems, setActiveItems }) => {
       p-[8px] pt-[8px] pb-[20px] 
         desktop:p-[20px] desktop:pt-[20px] desktop:pb-[40px]"
       >
-        {items.map((item) => (
+        {/* {items.map((item) => (
           <button
             key={item.program_type}
             className={`pt-[8px] pb-[20px] rounded-full border ${activeItems.includes(item.program_type)
@@ -39,7 +39,29 @@ const TypeFilter: React.FC<Props> = ({ activeItems, setActiveItems }) => {
           >
             {item.label}
           </button>
-        ))}
+        ))} */}
+        <div className="flex gap-[8px] ml-2 mt-[8px] mb-[20px] h-[32px] justify-center items-center text-xs font-pretendard">
+          <button
+            className={`w-[55px] h-[32px] rounded-full font-semibold text-xs border ${
+              activeItems.includes('CHALLENGE')
+                ? 'bg-Primary-10 text-Primary-100 border-Primary-50'
+                : 'bg-white text-Neutral-grayscale-40 border-Neutral-grayscale-80'
+            }`}
+            onClick={() => handleClick('CHALLENGE')}
+          >
+            챌린지
+          </button>
+          <button
+            className={`w-[81px] h-[32px] rounded-full font-semibold text-xs border ${
+              activeItems.includes('LIVECLASS')
+                ? 'bg-Primary-10 text-Primary-100 border-Primary-50'
+                : 'bg-white text-Neutral-grayscale-40 border-Neutral-grayscale-80'
+            }`}
+            onClick={() => handleClick('LIVECLASS')}
+          >
+            LIVE 클래스
+          </button>
+        </div>
       </div>
     </div>
   );
