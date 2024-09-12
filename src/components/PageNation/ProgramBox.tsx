c407be737edc2abd4507022e93db7c405d66d8a2
nebulaBdj <dongdung5070@gmail.com>
2024-09-12 09:56:08 +0000

    !HOTFIX: 사용하지 않는 import 제거

--- src/components/PageNation/ProgramBox.tsx
+++ src/components/PageNation/ProgramBox.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import RecruitTags, { RecruitState } from '../Filters/RecruitTags';
 import CareerTags from '../Filters/CareerTags';
